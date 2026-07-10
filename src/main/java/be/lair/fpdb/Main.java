e0086c5135ac8209864b8167fceccffffe95c4ed
Bert Geens <bert@lair.be>
2026-07-10 13:32:51 +0200

    set up project baseline

---
+++ src/main/java/be/lair/fpdb/Main.java
@@ -0,0 +1,29 @@
+// FPDB
+// Copyright (C) 2026 Bert Geens
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+package be.lair.fpdb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class Main {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    void main() {
+        log.info("Starting FPDB...");
+    }
+}
